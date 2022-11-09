db58529d062d7028368fe9233777a4d4304bfc4b
0sol0 <jdsmy1004@gmail.com>
2022-11-09 16:00:47 +0900

    add user app

--- sparta/views.py
+++ sparta/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
 def home(request):
     if request.method == 'GET':
+        if request.user.is_annonymous:
+            return redirect('/login/')
+        
         return render(request, 'sparta/home.html') 
--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from pickle import NONE
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from . import models
 
 # Create your views here.
@@ -8,36 +9,43 @@ def signup(request):
     if request.method == 'GET':
         user = request.user.is_authenticated
         if user:
-            return redirect('/home')
+            return redirect('/home/')
         else: 
             return render(request, 'user/signup.html')
         
     elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        phone = request.POST.get('phone', None)
-        address = request.POST.get('address', None)
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        phone = request.POST.get('phone', '')
+        address = request.POST.get('address', '')
         
-        exist_user = models.User().objects.filter(username=username)
+        exist_user = models.User.objects.filter(username=username)
         
         if exist_user:
             return render(request, 'user/signup.html')
+        
         else:
             models.User.objects.create_user(username=username, password=password, phone=phone, address=address)
-            return redirect('/login')
+            return redirect('/login/')
 
         
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'user/login.html')
-    
-    elif request.method == 'POST':
-        username = request.POST.get('username', NONE)
-        password = request.POST.get('password', NONE)
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
         
-        me = models.User.objects.get(username=username)
-        if me.password == password:
-            return redirect('/home')
-        else:
-            return redirect('/login')
-            
+        user = authenticate(request, username=username, password=password)
+        
+        if user:
+            login(request, user)
+            return redirect('/home/')
+        
+        return render(request,'user/login.html')
+        
+    elif request.method == 'GET':
+        user = request.user.is_authenticated
+        
+        if user:
+             return redirect('/home/')
+        
+        return render(request,'user/login.html')
